315a8da9bb56d8c947e51ea3d01d0896b7ae5247
loloje <lorenzo33laurent@hotmail.com>
2016-12-12 18:09:23 +0100

    layout avec header et fichier css du header correspondant

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -2,14 +2,78 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title><?= $this->e($title) ?></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title><?= $this->e($title) ?></title>
 
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="css/agency.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/styles.css') ?>">
 </head>
-<body>
+<body id="page-top" class="index">
+
 	<div class="container">
 		<header>
+
 			<h1><?= $this->e($title) ?></h1>
+
+			<div id="top-bar">
+            	<div id="logo">Logo</div>
+            	<div id="name-site">LivrEchange</div>
+    		</div>
+
+			<!-- Navigation -->
+    	<nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            	<div class="navbar-header page-scroll">
+                	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    	<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                	</button>
+                		<a class="navbar-brand page-scroll" href="#page-top">Accueil</a>
+            	</div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Mon profil</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Recherche</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Lieux d'échange</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#team">Actu</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Dons</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Se connecter</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        	</div>
+        	<!-- /.container-fluid -->
+    	</nav>
 		</header>
 
 		<section>
