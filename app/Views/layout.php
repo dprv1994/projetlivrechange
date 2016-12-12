664855512f82172558a3197c2443922fd94beb11
loloje <lorenzo33laurent@hotmail.com>
2016-12-12 19:28:24 +0100

    Ajout footer au layout et css correspondant

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -81,6 +81,27 @@
 		</section>
 
 		<footer>
+            <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Copyright &copy; Your Website 2016</span>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    
+                        <a href="#">Nous contacter</a>
+                </div>
+            </div>
+            </div>
 		</footer>
 	</div>
 <script
