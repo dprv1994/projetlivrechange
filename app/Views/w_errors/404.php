d4dc786eed05f79d18409981e8b832d3f3f5db43
Zaerowolf <ldarruau@gmail.com>
2016-12-12 14:55:21 +0100

    404
    image et texte de la page 404

--- app/Views/w_errors/404.php
+++ app/Views/w_errors/404.php
@@ -1,5 +1,9 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+<?php $this->layout('layout', ['title' => 'Page n°404']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404. Perdu ?</h1>
+<h1>Mais où est-elle donc passée..?</h1>
+<br>
+<!-- INTEGRER EN BACKGROUND L'IMAGE assets/img/404.jpg -->
+<p>Il semblerait que la page que vous recherchez ait été supprimée, désactivée ou déplacée.
+<br></p>
 <?php $this->stop('main_content'); ?>
