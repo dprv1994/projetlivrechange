fff2e311f4355b65c482ab989353fff86b8928eb
dprv1994 <dprv.1994@gmail.com>
2016-12-13 17:01:00 +0100

    controller user 1700

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -6,6 +6,9 @@ use \W\Controller\Controller;
 use \W\Model\UsersModel; //as Users;
 use \W\Security\AuthentificationModel;
 
+// Si on utilise "respect/validation". Ne pas oublier de l'ajouter via composer
+use \Respect\Validation\Validator as v;
+
 
 class UsersController extends Controller 
 {
@@ -83,11 +86,90 @@ class UsersController extends Controller
 		}
 	}
 
+	/**
+	 *Méthode pour verifier l'inscrtipon de l'utilisateur
+	 * Page d'inscription
+	**/
+
 	public function signIn()
 	{
 
+	$UsersModel = new UsersModel();
 
+	$errors = [];
+	$post = [];
+	$success = false;
 
+		if (!empty($_POST)) {
+			$post = array_map('trim', array_map('strip_tags',$_POST));
+		
+			if (!v::length(3, 25)->validate($post['firstname'])) {
+				$errors[] = 'Votre prénom doit faire entre 3 e 25 caractères';
+			}
+
+			if (!v::length(3, 25)->validate($post['lastname'])) {
+				$errors[] = 'Votre nom doit faire entre 3 e 25 caractères';
+			}
+
+			if (!v::length(6, 20)->validate($post['username'])) {
+				$errors[] = 'Votre nom d\'utilisateur doit faire entre 6 e 20 caractères';
+			}
+
+			//si l'username existe déjà en BDD renverra TRUE
+			if ($UsersModel->usernameExists($post['username'])) {
+				$errors[] = 'Le pseudo et déjà utilisé';
+			}
+
+			if(!v::image()->validate($_FILES['picture']['tmp_name'])) {
+				$errors[] = 'L\'image n\'est pas valide';
+			}
+
+			if (!v::email()->validate($post['email'])) {
+				$errors[] = 'Votre e-mail n\'est pas valide';
+			}
+
+			//si l'email existe déjà en BDD renverra TRUE
+			if ($UsersModel->emailExists($post['email'])) {
+				$errors[] = 'L\'adresse email et déjà utilisé';
+			}
+
+			if (!v::length(7,null)->validate($post['password'])) {
+				$errors[] = 'Le mot de passe doit avoir au moins 7 caractères';
+			}
+
+			if (count($errors) === 0 ) {
+				 
+				 $authModel = new AuthentificationModel(); // Permet d'utiliser la fonction de hash de password
+
+				 //On instancie le modèle pour communiquer avec la BDD
+				 $UserModel = new UserModel();
+
+				 $insert = $UserModel->insert( [
+				 	'firstname' => $post['firstname'],
+				 	'lastname'	=> $post['lastname'],
+				 	'username'	=> $post['username'],
+				 	'picture'	=> $post['picture'],
+				 	'email'		=> $post['email'],
+				 	'password'	=> $authModel->hashPassword($post['password']),
+				 	'role'			=> 'user',
+				 ]);
+
+				if ($insert) {
+					$success = true;
+				}
+				else{
+					$errors[] = 'Erreur lors de l\'ajout en BDD';
+				}
+			}
+		}
+
+		// Après le !empty($_POST) on envoi la vue et les éventuels paramètres
+		$params = [
+			'errors'  => $errors,
+			'success' => $success,
+		];
+
+		$this->show('default/signin', $params);
 	}
 }
 
