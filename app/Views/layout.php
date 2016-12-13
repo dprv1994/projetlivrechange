53ec0545da68cfcdc56e529d55778ec59371235a
loloje <lorenzo33laurent@hotmail.com>
2016-12-13 14:34:45 +0100

    en cours

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -81,8 +81,6 @@
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
                         <li><a href="#"><i class="fa fa-facebook"></i></a>
                         </li>
                         <li><a href="#"><i class="fa fa-linkedin"></i></a>
@@ -90,8 +88,9 @@
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    
+                    <div id="contact">
                         <a href="#">Nous contacter</a>
+                    </div> 
                 </div>
             </div>
             </div>
