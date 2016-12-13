3baa5651ff244984e23b63faa4db5f9da0e443bd
Zaerowolf <ldarruau@gmail.com>
2016-12-13 12:16:04 +0100

    ajout texte home
    Sans mise en page, contenu texte uniquement

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -5,7 +5,31 @@
 	<h2>BIENVENUE SUR LIVRECHANGE</h2>
 
 
-	
+	<h4>1) Echanger</h4>
+
+	<p>Votre bibliothèque déborde de livres que vous ne lirez plus?
+	Vous avez envie de découvrir un livre inattendu?
+	Nous vous proposons le contrat suivant : 1 livre donné = 1 livre reçu !<br>
+	Composé uniquement de particuliers, notre site vous propose de lister les livres que vous souhaitez proposer à l'échange, ainsi que ceux que vous recherchez, afin de trouver le parfait troc pour vous!</p>
+
+	<p>------------------------</p>
+
+	<h4>2) Se rencontrer</h4>
+
+	<p>LivrEchange vous propose de rencontrer "dans la vraie vie" les propriétaires des livres qui vous intéressent.<br>
+	Aucun système de livraison n'est proposé sur notre site.<br>
+	Pourquoi? Dans une époque où les gens n'intéragissent principalement que par l'intermédiaire des machines, nous pensons qu'il est important de continuer à échanger "en direct"..<br>
+	C'est pourquoi nous vous proposons des lieux partenaires (cafés, bars..) qui vous permettront de retrouver facilement la personne avec qui vous avez rendez-vous, et qui vous offriront un cadre agréable pour discuter le temps de votre transaction.</p>
+
+	<button>S'INSCRIRE</button>
+
+	<h4>3) Concept</h4>
+
+	<p>LivrEchange vous permet d'êter mis en contact et d'échanger vos livres facilement avec les personnes de votre région !<br>
+	Inscrivez-vous, remplissez votre profil, et partez à la recherche d'un échange intéressant !<br>
+	Grâce à notre partenariat avec de nombreux cafés locaux, retrouvez sans difficulté la personne avec qui vous devez échanger.<br>
+	Il vous suffit de jeter un oeil à notre <a href="">carte des lieux</a> pour trouver le point de rendez-vous le plus proche de chez vous!<br>
+	Et pourquoi ne pas en profiter pour boire un bon café en profitant de votre nouveau livre? ;)</p>
 
 
 <?php $this->stop('main_content') ?>
