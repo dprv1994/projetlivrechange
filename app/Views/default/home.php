0469875ff0b76d43fd56056b16b5bc8947c0fdd5
loloje <lorenzo33laurent@hotmail.com>
2016-12-13 17:19:36 +0100

    re

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -4,41 +4,42 @@
 
 <main>
 	
-	<section id="zone1">
+	<h1>BIENVENUE SUR LIVRECHANGE</h1>
 
-		<h1>BIENVENUE SUR LIVRECHANGE</h1>
+		<section id="zone1">
 
+			<h3>1) Echanger</h3>
 
-	<h3>1) Echanger</h3>
-
-	<p>Votre bibliothèque déborde de livres que vous ne lirez plus?
-	Vous avez envie de découvrir un livre inattendu?
-	Nous vous proposons le contrat suivant : 1 livre donné = 1 livre reçu !<br>
-	Composé uniquement de particuliers, notre site vous propose de lister les livres que vous souhaitez proposer à l'échange, ainsi que ceux que vous recherchez, afin de trouver le parfait troc pour vous!</p>
+				<p>Votre bibliothèque déborde de livres que vous ne lirez plus?
+				Vous avez envie de découvrir un livre inattendu?
+				Nous vous proposons le contrat suivant : 1 livre donné = 1 livre reçu !<br>
+				Composé uniquement de particuliers, notre site vous propose de lister les livres que vous souhaitez proposer à l'échange, ainsi que ceux que vous recherchez, afin de trouver le parfait troc pour vous!</p>
 
 	
 
-	<h3>2) Se rencontrer</h3>
+			<h3>2) Se rencontrer</h3>
 
-	<p>LivrEchange vous propose de rencontrer "dans la vraie vie" les propriétaires des livres qui vous intéressent.<br>
-	Aucun système de livraison n'est proposé sur notre site.<br>
-	Pourquoi? Dans une époque où les gens n'intéragissent principalement que par l'intermédiaire des machines, nous pensons qu'il est important de continuer à échanger "en direct"..<br>
-	C'est pourquoi nous vous proposons des lieux partenaires (cafés, bars..) qui vous permettront de retrouver facilement la personne avec qui vous avez rendez-vous, et qui vous offriront un cadre agréable pour discuter le temps de votre transaction.</p>
+			<p>LivrEchange vous propose de rencontrer "dans la vraie vie" les propriétaires des livres qui vous intéressent.<br>
+			Aucun système de livraison n'est proposé sur notre site.<br>
+			Pourquoi? Dans une époque où les gens n'intéragissent principalement que par l'intermédiaire des machines, nous pensons qu'il est important de continuer à échanger "en direct"..<br>
+			C'est pourquoi nous vous proposons des lieux partenaires (cafés, bars..) qui vous permettront de retrouver facilement la personne avec qui vous avez rendez-vous, et qui vous offriront un cadre agréable pour discuter le temps de votre transaction.</p>
 
-	</section>
+		</section>
 
 
-	<button>S'INSCRIRE</button>
+		<button>S'INSCRIRE</button>
 
-	<h3>3) Concept</h3>
+		<section id="zone2">
 
-	<p>LivrEchange vous permet d'êter mis en contact et d'échanger vos livres facilement avec les personnes de votre région !<br>
-	Inscrivez-vous, remplissez votre profil, et partez à la recherche d'un échange intéressant !<br>
-	Grâce à notre partenariat avec de nombreux cafés locaux, retrouvez sans difficulté la personne avec qui vous devez échanger.<br>
-	Il vous suffit de jeter un oeil à notre <a href="">carte des lieux</a> pour trouver le point de rendez-vous le plus proche de chez vous!<br>
-	Et pourquoi ne pas en profiter pour boire un bon café en profitant de votre nouveau livre? ;)</p>
+			<h3>3) Concept</h3>
 
-	</div>
+				<p>LivrEchange vous permet d'êter mis en contact et d'échanger vos livres facilement avec les personnes de votre région !<br>
+				Inscrivez-vous, remplissez votre profil, et partez à la recherche d'un échange intéressant !<br>
+				Grâce à notre partenariat avec de nombreux cafés locaux, retrouvez sans difficulté la personne avec qui vous devez échanger.<br>
+				Il vous suffit de jeter un oeil à notre <a href="">carte des lieux</a> pour trouver le point de rendez-vous le plus proche de chez vous!<br>
+				Et pourquoi ne pas en profiter pour boire un bon café en profitant de votre nouveau livre? ;)</p>
+
+		</section>
 
 </main>
 <?php $this->stop('main_content') ?>
