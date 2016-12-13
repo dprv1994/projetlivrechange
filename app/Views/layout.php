eedbd72233b36123f2946f0d3ddce4894913d930
loloje <lorenzo33laurent@hotmail.com>
2016-12-13 15:46:37 +0100

    1

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -38,7 +38,7 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             	<div class="navbar-header page-scroll">
                 	<a class="navbar-brand page-scroll" href="">Accueil</a>
-            	</div>
+            </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
