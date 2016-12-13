65d2a38ba9d70feed312caee2080c9d3f50b180a
Zaerowolf <ldarruau@gmail.com>
2016-12-13 10:40:01 +0100

    1

---
+++ app/Views/default/loginuser.php
@@ -0,0 +1,9 @@
+<?php $this->layout('layoutBack', ['title' => 'Page n°404']) ?>
+
+<?php $this->start('main_content'); ?>
+<h1>Mais où est-elle donc passée..?</h1>
+<br>
+<!-- INTEGRER EN BACKGROUND L'IMAGE assets/img/404.jpg -->
+<p>Il semblerait que la page que vous recherchez ait été supprimée, désactivée ou déplacée.
+<br></p>
+<?php $this->stop('main_content'); ?>
