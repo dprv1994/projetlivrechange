a31cbe919dc85d438ad857802ec5375294673206
dprv1994 <dprv.1994@gmail.com>
2016-12-14 11:39:25 +0100

    search JQuery

--- app/Views/default/search.php
+++ app/Views/default/search.php
@@ -10,7 +10,40 @@
 
 	<div id="results"></div>
 
-<!-- SCRIPTS JS DE LA PAGE -->
+<!-- SCRIPTS JQuery DE LA PAGE -->
+
+<script>
+	function bookSearch(){
+	var search = document.getElementById('search').value
+	document.getElementById('results').innerHTML = ""
+	console.log(search);
+
+	$.ajax({
+		url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+		dataType:"json",
+
+		success:function(data){
+			for(i = 0; i < data.items.length; i++){
+				results.innerHTML += "<h2>Title:" + data.items[i].volumeInfo.title + "</h2>"
+				results.innerHTML += "<h3>Authors:" + data.items[i].volumeInfo.authors  + "</h3>" 
+				results.innerHTML += "<h4>Publisher:" + data.items[i].volumeInfo.publisher  + "</h4>"  
+				results.innerHTML += "<p>published Date:" + data.items[i].volumeInfo.publishedDate + "</p>"
+				results.innerHTML += "<p>Categories:" + data.items[i].volumeInfo.categories + "</p>"
+				results.innerHTML += "<p>img:" + '<img src="' + data.items[i].volumeInfo.imageLinks.thumbnail + '">' + "</p>"
+			}
+		},
+
+		type:'GET'
+	});
+}
+
+document.getElementById('submit').addEventListener('click', bookSearch, false);
+
+
+	
+</script>
+
+
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 	<script src="js/myscript.js"></script>
