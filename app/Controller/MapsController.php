fa6b20f76d0ecb70428d146413627e5d20881fb0
hobbyon <romain.michler@hotmail.com>
2016-12-14 17:57:29 +0100

    supression du var dump casse burne

--- app/Controller/MapsController.php
+++ app/Controller/MapsController.php
@@ -3,7 +3,6 @@
 namespace Controller;
 
 use \W\Controller\Controller;
-<<<<<<< HEAD
 use Model\MapsModel; 
 
 
@@ -11,28 +10,12 @@ use Model\MapsModel;
 
 class MapsController extends Controller
 {
-=======
-use \W\Security\AuthentificationModel;
-use Model\MapsModel; 
-
-
-	class MapsController 
-	{
-
-	/**
-	* Page Maps
-	*/
-	public function showMaps()
-	{
-		$this->show('/default/maps');
->>>>>>> origin/master
 
 		public function getMakers()
 		{
 			$MapsModel = new MapsModel();
 			$markers = $MapsModel->findAll();
 		
-			var_dump($markers);
 			$data = [
 				'markers' => $markers
 			];
