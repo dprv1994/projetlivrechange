0c7b6e55a9482b82175643f282cc83400de09458
loloje <lorenzo33laurent@hotmail.com>
2016-12-14 09:15:06 +0100

    re

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -9,21 +9,23 @@
 		<section id="zone1">
 
 			<h3>1) Echanger</h3>
-
+			<div id="container-colonnes">
+				<div id="colonne1">
 				<p>Votre bibliothèque déborde de livres que vous ne lirez plus?
 				Vous avez envie de découvrir un livre inattendu?
 				Nous vous proposons le contrat suivant : 1 livre donné = 1 livre reçu !<br>
 				Composé uniquement de particuliers, notre site vous propose de lister les livres que vous souhaitez proposer à l'échange, ainsi que ceux que vous recherchez, afin de trouver le parfait troc pour vous!</p>
-
+				</div>
 	
 
 			<h3>2) Se rencontrer</h3>
-
+				<div id="colonne2">
 				<p>LivrEchange vous propose de rencontrer "dans la vraie vie" les propriétaires des livres qui vous intéressent.<br>
 				Aucun système de livraison n'est proposé sur notre site.<br>
 				Pourquoi? Dans une époque où les gens n'intéragissent principalement que par l'intermédiaire des machines, nous pensons qu'il est important de continuer à échanger "en direct"..<br>
 				C'est pourquoi nous vous proposons des lieux partenaires (cafés, bars..) qui vous permettront de retrouver facilement la personne avec qui vous avez rendez-vous, et qui vous offriront un cadre agréable pour discuter le temps de votre transaction.</p>
-
+				</div>
+			</div>
 		</section>
 
 
