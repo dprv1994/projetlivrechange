0c741a0fd5b2baaacd15c1c52b9152f09ad0ec42
loloje <lorenzo33laurent@hotmail.com>
2016-12-14 11:08:30 +0100

    re

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -80,11 +80,15 @@
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
                         <li><a href="#"><i class="fa fa-facebook"></i></a>
                         </li>
                         <li><a href="#"><i class="fa fa-linkedin"></i></a>
                         </li>
                     </ul>
+                        
+                   
                 </div>
                 <div class="col-md-4">
                     <div id="contact">
