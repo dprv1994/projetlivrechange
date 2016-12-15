c4339a186691cfad7745ba965df42e52060dca6e
dprv1994 <dprv.1994@gmail.com>
2016-12-15 14:29:02 +0100

    tj

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -33,10 +33,7 @@
                 	<div class="navbar-header page-scroll">
                     	<a class="navbar-brand page-scroll" href="/projetlivrechange/public/">Accueil</a>
                     </div>
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
                 <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
