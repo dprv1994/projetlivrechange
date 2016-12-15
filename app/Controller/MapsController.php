71883e846ac411b3949988ea6c6fe5634bbd6d50
hobbyon <romain.michler@hotmail.com>
2016-12-15 12:49:04 +0100

    maps

--- app/Controller/MapsController.php
+++ app/Controller/MapsController.php
@@ -4,6 +4,7 @@ namespace Controller;
 
 use \W\Controller\Controller;
 use Model\MapsModel; 
+use \W\View\Plates\PlatesExtensions;
 
 
 
@@ -22,5 +23,5 @@ class MapsController extends Controller
 			$this->show('default/maps', $data); // envoie les données en paramètres 
 		}
 
-
+		
 }
--- app/Views/default/maps.php
+++ app/Views/default/maps.php
@@ -41,18 +41,42 @@
 
 				
 	             //Création de l'icone
-	             var myMarkerImage = new google.maps.Marker;
-	                  
-	             //Affichage du marqueur
-	             for(i=0;i<locations.length;i++) { 
-				 	marker = new google.maps.Marker({
-				 	position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-				 	map: map,
+	            var image = {
+				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
+				  size: new google.maps.Size(225, 157),
+				  origin: new google.maps.Point(0, 0),
+				  anchor: new google.maps.Point(17, 34),
+				  scaledSize: new google.maps.Size(50, 60)
+				};
+	            
+				    
+	    	     //Affichage du marqueur
+	            for(i=0;i<locations.length;i++) { 
+				   marker = new google.maps.Marker({
+				   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+				   map: map,
+				   icon: image, 
+				   
 					
-					});
+				   });
+				    // Création infobulle  
+	            	var infowindow = new google.maps.InfoWindow();  
+
+					
+					google.maps.event.addListener(marker, 'click', (function(marker, i) {
+						return function() {
+							infowindow.setContent('Echangez ! <b>' + locations[i][0]);
+							infowindow.open(map, marker);
+						}
+						})(marker, i));
+				
+					 
+
 				};
 				  
 			}
+			
+	  
 	  </script>
 
 
