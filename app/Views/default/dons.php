2e2d424bf7c9a108c198a1bafcf48160ff2ca22a
Zaerowolf <ldarruau@gmail.com>
2016-12-16 11:46:01 +0100

    123

--- app/Views/default/dons.php
+++ app/Views/default/dons.php
@@ -4,8 +4,12 @@
 
 <?php $this->start('main_content') ?>
 <br><br>
-https://www.recyclivre.com/donner-ses-livres.php
-<br>
+<body>
+	<h2>Le livre, source d'éducation et d'échange</h2>
+	<p>Dans le cadre de notre collaboration avec le site Recyclivre, nous vous proposons de
+	<br>Vous souhaitez faire don de vos vieux livres à des associations? Cliquez <a href="https://www.recyclivre.com/donner-ses-livres.php">ici</a> et participez au projet de notre partenaire.
+	<br>Donnez vos livres pour une bonne cause !</p>
+</body>
 
 <br><br><br><br><br>
 
