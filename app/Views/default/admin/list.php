19cac1b2b4cea32063f125cfcca7cb7f0658fadd
Zaerowolf <ldarruau@gmail.com>
2016-12-16 16:09:59 +0100

    modificatiosn listUsers BACK

--- app/Views/default/admin/list.php
+++ app/Views/default/admin/list.php
@@ -7,48 +7,56 @@
 <?php $this->layout('layoutBack', ['title' => 'Liste des utilisateurs ']) ?>
 
 <?php $this->start('main_content') ?>
-<?php include_once 'navbar.php'; ?>
+	
+	<div id="list_users">
 
-	<table class="table">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Prénom</th>
-				<th>Nom</th>
-				<th>nom de l'utilisateur</th>
-				<th>Email</th>
-				<th>Rôle</th>
-				<th>Profil</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php foreach ($users as $user): ?>		
-			<tr>
-				<td><?=$user['id'];?></td>	
-				<td><?=$user['firstname'];?></td>
-				<td><?=$user['lastname'];?></td>
-				<td><?=$user['username'];?></td>
-				<td><?=$user['email'];?></td>
-				<td><?=$user['role'];?></td>
-				<td>
-				<!-- Url vers le profil du membre -->
-				<a href="<?=$this->url('profilBack', ['id' => $user['id']]);?>">
-				Voir le profil</a>
-				</td>
-				<td>
-				<!-- Créer un url pour effacer un membre -->
-				<a href="<?=$this->url('user_update', ['id' => $user['id']]);?>">
-				Modifier le profil</a>
-				</td>
-				<td>-
-				<!-- Créer un url pour effacer un membre -->
-				<a href="<?=$this->url('user_delete', ['id' => $user['id']]);?>">
-				Effacer le profil</a>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
-	</table>
+		<h2><i class="fa fa-address-card" aria-hidden="true"></i>
+
+Liste des utilisateurs</h2>
+		<br>
+
+	<!-- tableeau contenant les utilisateurset permettant d'accéder aux options -->
+		<table class="table">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Prénom</th>
+					<th>Nom</th>
+					<th>nom de l'utilisateur</th>
+					<th>Email</th>
+					<th>Rôle</th>
+					<th>Profil</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($users as $user): ?>		
+				<tr>
+					<td><?=$user['id'];?></td>	
+					<td><?=$user['firstname'];?></td>
+					<td><?=$user['lastname'];?></td>
+					<td><?=$user['username'];?></td>
+					<td><?=$user['email'];?></td>
+					<td><?=$user['role'];?></td>
+					<td>
+					<!-- Url vers le profil du membre -->
+					<a href="<?=$this->url('profilBack', ['id' => $user['id']]);?>">
+					Voir le profil</a>
+					</td>
+					<td>
+					<!-- Créer un url pour effacer un membre -->
+					<a href="<?=$this->url('user_update', ['id' => $user['id']]);?>">
+					Modifier le profil</a>
+					</td>
+					<td>-
+					<!-- Créer un url pour effacer un membre -->
+					<a href="<?=$this->url('user_delete', ['id' => $user['id']]);?>">
+					Effacer le profil</a>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
 
 <?php $this->stop('main_content') ?>
