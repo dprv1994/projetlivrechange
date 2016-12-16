6e58a8681701f0ef7fe8f4b136b1ac85537a6347
hobbyon <romain.michler@hotmail.com>
2016-12-16 19:01:58 +0100

    coco

--- app/Views/default/admin/updateMaps.php
+++ app/Views/default/admin/updateMaps.php
@@ -1,4 +1,31 @@
+<?php
 
+
+
+$address = 'bordeaux';
+ 
+// On prépare l'URL du géocodeur
+$geocoder = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false";
+ 
+// Pour cette exemple, je vais considérer que ma chaîne n'est pas
+// en UTF8, le géocoder ne fonctionnant qu'avec du texte en UTF8
+$url_address = utf8_encode($address);
+ 
+// Penser a encoder votre adresse
+$url_address = urlencode($url_address);
+ 
+// On prépare notre requête
+$query = sprintf($geocoder,$url_address);
+ 
+// On interroge le serveur
+$results = file_get_contents($query);
+ 
+// On affiche le résultat
+var_dump(json_decode($results));
+
+
+
+?>
 <?php $this->layout('layoutBack', ['title' => 'Gestion de la carte google maps']) ?>
 
 <?php $this->start('main_content') ?>
@@ -10,6 +37,16 @@
 
  	<hr>
  	<br>
+
+ 	<h3>ajouter un nouveau lieu d'échange</h3>
+
+ 	<form method="POST">
+		<label for="adress"></label>
+		<input type="text" name="adress">
+		<input type="submit" name="envoyer">
+
+	</form>
+
 
  	<h3>Liste des points d'échange</h3>
 
@@ -50,8 +87,15 @@
 			function initMap() {
 			  	
 			  	/* --------------AFFICHAGE DE LA MAP--------------- */
-
+			  	var geocoder;
 			  	var map;
+			  	var image = {
+				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
+				  size: new google.maps.Size(100, 100),
+				  origin: new google.maps.Point(0, 0),
+				  anchor: new google.maps.Point(17, 34),
+				  scaledSize: new google.maps.Size(40, 60),
+				};
 			  	
 			  	map = new google.maps.Map(document.getElementById('mapupdate'), {/* Ici on crée notre objet map*/
 				    center: {lat: 44.8404400, lng:-0.5805000}, // centrage de la carte à l'affichage
@@ -60,53 +104,12 @@
 				
 			  	/* --------------AFFICHAGE DES MARQUEURS--------------- */
 
-
-
-				var locations = [
-					<?php foreach($markers as $marker): ?>
-						['<?=$marker['title'];?>', '<?=$marker['lat'];?>', '<?=$marker['lng'];?>'],
-					<?php endforeach; ?>
-				];
-
 				
 	             //Création de l'icone
-	            var image = {
-				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
-				  size: new google.maps.Size(100, 100),
-				  origin: new google.maps.Point(0, 0),
-				  anchor: new google.maps.Point(17, 34),
-				  scaledSize: new google.maps.Size(40, 60),
-				};
+	         }   
 	            
-				    
-	    	     //Affichage du marqueur
-	            for(i=0;i<locations.length;i++) { 
-				   marker = new google.maps.Marker({
-				   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-				   map: map,
-				   icon: image, 
-				   
-					
-				   });
-				 // Création infobulle  
-	            	var infowindow = new google.maps.InfoWindow();  
-
-					
-					google.maps.event.addListener(marker, 'click', (function(marker, i) {
-						return function() {
-							infowindow.setContent('Echangez ! <b>' + locations[i][0]);
-							infowindow.open(map, marker);
-						}
-						})(marker, i));
-				
-					 
-
-				};
-				  
-			}
-			
-	  
-	  </script>
+			   
+	    	   </script>
 
 
 	  			<!-- .......................FIN DU SCRIPT MAPS................................... -->
