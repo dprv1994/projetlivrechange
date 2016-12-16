ef7d9284035731572a69f22179f9eed13e2f3fa3
Zaerowolf <ldarruau@gmail.com>
2016-12-16 12:08:40 +0100

    maj dons et css

--- app/Views/default/dons.php
+++ app/Views/default/dons.php
@@ -6,9 +6,18 @@
 <br><br>
 <body>
 	<h2>Le livre, source d'éducation et d'échange</h2>
-	<p>Dans le cadre de notre collaboration avec le site Recyclivre, nous vous proposons de
-	<br>Vous souhaitez faire don de vos vieux livres à des associations? Cliquez <a href="https://www.recyclivre.com/donner-ses-livres.php" target="_blanck">ici</a> et participez au projet de notre partenaire.
-	<br>Donnez vos livres pour une bonne cause !</p>
+	
+	<br><br>
+
+	<div id="texte">
+		<p>RecycLivre est un site qui collecte -entre autres- les livres d'occasion dont vous souhaitez vous séparer. Ils interviennent dans différentes villes, et viennent les chercher chez vous gratuitement.
+		<br>Ils revendent les livres récoltés, afin de soutenir des programmes d'éducation dans des régions du monde qui en ont besoin. 
+		<br>Vous souhaitez faire don de vos vieux livres?
+	</div>
+
+	<br>
+	
+	<strong>Rendez-vous <a href="https://www.recyclivre.com/donner-ses-livres.php" target="_blanck">ici</a> pour participer à leur projet !</strong></p>
 </body>
 
 <br><br><br><br><br>
