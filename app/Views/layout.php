90e68c7a056e75757570be7d308f96ef54cd56f5
loloje <lorenzo33laurent@hotmail.com>
2016-12-16 16:20:51 +0100

    re

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale1.0, user-scalable=no">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <title><?= $this->e($title) ?></title>
