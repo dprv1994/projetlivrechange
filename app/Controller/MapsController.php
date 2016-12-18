504995855f01cd8bffa0771cbb7b2d9fb9533b81
hobbyon <romain.michler@hotmail.com>
2016-12-18 16:01:18 +0100

    controller add marker

--- app/Controller/MapsController.php
+++ app/Controller/MapsController.php
@@ -34,6 +34,52 @@ class MapsController extends Controller
 			$this->show('default/admin/updateMaps', $data); // envoie les données en paramètres 
 		
 		}
+		
+		public function addMarker()
+		{
+
+
+		$errors = [];
+		$post = [];
+		$success = false;
+
+		if (!empty($_POST)) {
+			$post = array_map('trim', array_map('strip_tags',$_POST));
+		
+			if (!v::length(3, 100)->validate($post['title'])) {
+				$errors[] = 'Votre prénom doit faire entre 3 e 25 caractères';
+			}
+
+			
+				 //On instancie le modèle pour communiquer avec la BDD
+				 $UserModel = new MapsModel();
+
+				 $insert = $UserModel->insert( [
+				 	'lat' => $post['lat'],
+				 	'lng'	=> $post['lng'],
+				 	
+			
+				 ]);
+
+				if ($insert) {
+					$success = true;
+				}
+				else{
+					$errors[] = 'Erreur lors de l\'ajout en BDD';
+				}
+			}
+		}
+
+		// Après le !empty($_POST) on envoi la vue et les éventuels paramètres
+		$params = [
+			'errors'  => $errors,
+			'success' => $success,
+		];
+
+		$this->show('default/admin/updateMaps', $params);
+	}
+
+
 		public function deleteMarker($id)
 		{
 			if (!is_numeric($id) || empty($id)) {
