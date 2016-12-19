3dc91fe86b1375ebbe7bae57e3a053bf0440c279
loloje <lorenzo33laurent@hotmail.com>
2016-12-19 17:37:47 +0100

    re

--- app/Views/default/contact.php
+++ app/Views/default/contact.php
@@ -2,34 +2,38 @@
 
 <?php $this->start('main_content') ?>
 
+<div class="form">
+
     <h1>LAISSEZ-NOUS UN MESSAGE</h1>
 
-<br><br>
-<form method="post">
+        <br><br>
+        <form method="post">
 
-
-    <label for="lastname">Nom :</label>
-        <input id="lastname" type="text" name="lastname" placeholder="Votre nom..." />
-    <br><br>
+            <label for="lastname">Nom :</label>
+                <input id="lastname" type="text" name="lastname" placeholder="Votre nom..." />
+        <br><br>
    
    
-    <label for="email">Email :</label>
-        <input id="email" type="email" name="email" placeholder="Ex : johndoe@gmail.com" />
-    <br><br>
+            <label for="email">Email :</label>
+                <input id="email" type="email" name="email" placeholder="Ex : johndoe@gmail.com" />
+        <br><br>
    
-    <label for="message">Message :</label>
-        <textarea id="message" name="message" placeholder="Votre message..."></textarea>
-    <br><br>
+        <label for="message">Message :</label>
+            <textarea id="message" name="message" placeholder="Votre message..."></textarea>
+        <br><br>
     
-    <label for="object">Objet :</label>
-        <input id="object" type="text" name="object" placeholder="Titre de votre message...">
-    <br><br>
+        <label for="object">Objet :</label>
+            <input id="object" type="text" name="object" placeholder="Titre de votre message...">
+        <br><br>
       
      
         <div class="btnsubmit">
-        <input type="submit" class="linebuttons" value="Envoyer" />
+            <input type="submit" class="linebuttons" value="Envoyer" />
         </div>
-    <br><br>
-</form>
+        <br><br>
+
+        </form>
+        
+</div>
 
 <?php $this->stop('main_content') ?>
