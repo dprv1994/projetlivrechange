7ba47678c2b040ac90c4b38481e608120488c7d1
Zaerowolf <ldarruau@gmail.com>
2016-12-20 09:28:48 +0100

    màj boutons réseaux sociaux
    un facebook et un twitter, avec href effectifs.
    Au vu du type de site que nous faisons c'est plus logique qu'un FB et un
    LinkedIn !

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -103,9 +103,9 @@
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        <li><a href="http://facebook.com" target="_blank"><i class="fa fa-facebook"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        <li><a href="http://twitter.com" target="_blank"><i class="fa fa-twitter"></i></a>
                         </li>
                     </ul>
                 </div>
