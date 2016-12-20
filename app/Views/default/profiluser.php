3d92414d63e9fe1b9f6f00725343393a25f94718
loloje <lorenzo33laurent@hotmail.com>
2016-12-20 13:00:19 +0100

    mise en page

--- app/Views/default/profiluser.php
+++ app/Views/default/profiluser.php
@@ -5,22 +5,27 @@
 <?php $this->start('main_content') ?>
 
 
-	<div class="container-colonnes">
+	
 		<?php if (!empty($user)): ?>
+	<div id="container-colonnes">
 
-			<div>
-				<h2>Mon profil</h2>
-
+		<div id="profil">
+			<h2>Mon profil :</h2>
 				<ul>
 					<li>Pseudo : <?=$user['username'];?></li>
 					<li>Prénom : <?=$user['firstname'];?></li>
 					<li>Nom : <?=$user['lastname'];?></li>
 					<li>Email : <?=$user['email'];?></li>
-					<li>Avatar: <img src="<?=$this->assetUrl($upload.$user['picture'])?>"> </li>
 				</ul>
-				<a href="">Modifier mon profil</a>
-			</div>
+		</div>
 
+		<div id="image"><h2>Avatar : <img src="<?=$this->assetUrl($upload.$user['picture'])?>"></h2> 
+		</div>
+				
+	</div>
+			<a href="">Modifier mon profil</a>
+
+			<br><br>
 			<div>
 				<h2>Mes livres :</h2>
 				<ul>
@@ -28,12 +33,13 @@
 				</ul>
 				<a href="<?=$this->url('add_book')?>">Ajouter un livre</a>
 			</div>
+			<br><br>
 
 		<?php else: ?>
 			<div class="alert alert-danger">
 				L'utilisateur n'existe pas
 			</div>
 		<?php endif; ?>
-	</div>	
+	
 
 <?php $this->stop('main_content') ?>
