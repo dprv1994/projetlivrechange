cc6fdca0297e1eec8d8824fdf54ac5c8afe55dbc
dprv1994 <dprv.1994@gmail.com>
2016-12-20 17:30:41 +0100

    1730

--- app/Model/BooksModel.php
+++ app/Model/BooksModel.php
@@ -6,9 +6,12 @@ class BooksModel extends \W\Model\Model
 
 	public function MyBooks()
 	{	
-		$select = $bdd->prepare('SELECT books.title, users.username FROM users
-		INNER JOIN books
-		ON books.id_users = users.id');
+
+		$sql = 'SELECT books.title, users.username FROM users
+				INNER JOIN books
+				ON books.id_users = users.id';
+				
+		$select = $bdd->prepare($sql);
 
 		if ($select->execute()) {
 			$books = $select->fetchAll(PDO::FETCH_ASSOC);
