9e35acb4fbf0cec168393c29d98c31450f4a2930
hobbyon <romain.michler@hotmail.com>
2016-12-21 13:59:12 +0100

    map front ok

--- app/Views/default/maps.php
+++ app/Views/default/maps.php
@@ -36,67 +36,54 @@
 
 
 <?php $this->start('js') ?>
+<script type="text/javascript">
 
-	  <script type="text/javascript">
-	  		
-
-			
-			function initMap() {
-			  	
-			  	/* --------------AFFICHAGE DE LA MAP--------------- */
-
-			  	var map;
-			  	
-			  	map = new google.maps.Map(document.getElementById('map'), {/* Ici on crée notre objet map*/
-				    center: {lat: 44.8404400, lng:-0.5805000}, // centrage de la carte à l'affichage
-				    zoom: 13 // niveau de zoom
-			  	});
+	function initMap() {
+	  	/* --------------AFFICHAGE DE LA MAP--------------- */
+	  	var map;
+	  	
+	  	map = new google.maps.Map(document.getElementById('map'), {/* Ici on crée notre objet map*/
+		    center: {lat: 44.8404400, lng:-0.5805000}, // centrage de la carte à l'affichage
+		    zoom: 13 // niveau de zoom
+	  	});
 				
-			  	/* --------------AFFICHAGE DES MARQUEURS--------------- */
+	  	/* --------------AFFICHAGE DES MARQUEURS--------------- */
+		var locations = [
+			<?php foreach($markers as $marker): ?>
+				['<?=$marker['title'];?>', '<?=$marker['lat'];?>', '<?=$marker['lng'];?>'],
+			<?php endforeach; ?>
+		];
 
+		
+	     //Création de l'icone
+	    var image = {
+		  url: "<?=$this->assetUrl('img/logo.png');?>" ,
+		  size: new google.maps.Size(100, 100),
+		  origin: new google.maps.Point(0, 0),
+		  anchor: new google.maps.Point(17, 34),
+		  scaledSize: new google.maps.Size(40, 60),
+		};
 
+		//Affichage du marqueur
+	    for(i=0;i<locations.length;i++) { 
+		   marker = new google.maps.Marker({
+		   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		   map: map,
+		   icon: image, 
+		});
 
-				var locations = [
-					<?php foreach($markers as $marker): ?>
-						['<?=$marker['title'];?>', '<?=$marker['lat'];?>', '<?=$marker['lng'];?>'],
-					<?php endforeach; ?>
-				];
-
+		// Création infobulle  
+	    var infowindow = new google.maps.InfoWindow();  
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+					infowindow.setContent('Echangez ! <b>' + locations[i][0]);
+					infowindow.open(map, marker);
+				}
+			})(marker, i));
 				
-	             //Création de l'icone
-	            var image = {
-				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
-				  size: new google.maps.Size(100, 100),
-				  origin: new google.maps.Point(0, 0),
-				  anchor: new google.maps.Point(17, 34),
-				  scaledSize: new google.maps.Size(40, 60),
-				};
-	            
-				    
-	    	     //Affichage du marqueur
-	            for(i=0;i<locations.length;i++) { 
-				   marker = new google.maps.Marker({
-				   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-				   map: map,
-				   icon: image, 
-				   
-					
-				});
-				 // Création infobulle  
-	            	var infowindow = new google.maps.InfoWindow();  
-
-					
-					google.maps.event.addListener(marker, 'click', (function(marker, i) {
-						return function() {
-							infowindow.setContent('Echangez ! <b>' + locations[i][0]);
-							infowindow.open(map, marker);
-						}
-					})(marker, i));
-				
-			}
-			
-	  
-	  </script>
+		}
+	}
+ </script>
 
 
 	  			<!-- .......................FIN DU SCRIPT MAPS................................... -->
