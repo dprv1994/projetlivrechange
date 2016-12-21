6f59cee20cb108ca7845784a8a681c5d2cc0e33e
loloje <lorenzo33laurent@hotmail.com>
2016-12-21 15:15:27 +0100

    1515

--- app/Views/default/admin/addUser.php
+++ app/Views/default/admin/addUser.php
@@ -17,47 +17,66 @@
 	<h2><i class="fa fa-user-circle" aria-hidden="true"></i>
  Ajouter un utilisateur</h2><br>
 
+<div class="add">
 	<div id="texte">
 	<form method="post" enctype="multipart/form-data">
 		
-		<label  for="firstname">Prénom:</label>
+		<label  for="firstname">Prénom :</label>
+		<div class="center">
 		<input type="text" id="firstname" name="firstname">
+		</div>
 		<br><br>
 
-		<label for="lastname">Nom:</label>
+		<label for="lastname">Nom :</label>
+		<div class="center">
 		<input type="text" id="lastname" name="lastname">
+		</div>
 		<br><br>
 
-		<label for="username">Pseudonyme:</label>
+		<label for="username">Pseudonyme :</label>
+		<div class="center">
 		<input type="text" id="username" name="username">
+		</div>
 		<br><br>
 
-		<label for="picture">Image:</label>
+		<label for="picture">Image :</label>
+		<div class="center">
 		<input type="file" id="picture" name="picture" placeholder="Votre image">
+		</div>
 		<br><br>
 
-		<label for="email">Email:</label>
+		<label for="email">Email :</label>
+		<div class="center">
 		<input type="email" id="email" name="email">
+		</div>
 		<br><br>
 
 
-		<label for="password">Mot de passe:</label>
+		<label for="password">Mot de passe :</label>
+		<div class="center">
 		<input type="password" id="password" name="password">
+		</div>
 		<br><br>
 
 		
-		<label for="role">role:</label>
+		<label for="role">role :</label>
+		<div class="center">
 		<select name="role">
 			<option>admin</option>
 			<option>modo</option>
 			<option>user</option>
 		</select>
+		</div>
+		<br><br>
 
-		<input type="submit"  value="Enregistrer">
-
+		<div class="btnsubmit">
+		<input type="submit"  class="linebuttons" value="ENREGISTRER">
+		</div>
+		<br><br>
 
 	</form>
 	</div>
+</div>
 
 
 <?php $this->stop('main_content') ?>
