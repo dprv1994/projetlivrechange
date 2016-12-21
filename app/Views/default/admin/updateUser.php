888b785309320f8c61f16c41c1d516b64783c2c4
loloje <lorenzo33laurent@hotmail.com>
2016-12-21 13:47:09 +0100

    1347

--- app/Views/default/admin/updateUser.php
+++ app/Views/default/admin/updateUser.php
@@ -4,48 +4,66 @@
 
 <?php $this->start('main_content') ?>
 
-	<h2>Modifier un utlisateur</h2>
+<h2>Modifier un utlisateur</h2>
 
-	<form method="POST">
+<br>
+<div class="add">
+	<form method="POST" enctype="multipart/form-data">
 		
-		<label  for="firstname">Prénom:</label>
+		<label  for="firstname">Prénom :</label>
+		<div class="center">
 		<input type="text" id="firstname" name="firstname">
+		</div>
 		<br><br>
 
-		<label for="lastname">Nom:</label>
+		<label for="lastname">Nom :</label>
+		<div class="center">
 		<input type="text" id="lastname" name="lastname">
+		</div>
 		<br><br>
 
-		<label for="username">Nom d'utilisateur:</label>
+		<label for="username">Pseudo :</label>
+		<div class="center">
 		<input type="text" id="username" name="username">
+		</div>
 		<br><br>
 
 		<label for="picture">Image:</label>
+		<div class="center">
 		<input type="file" id="picture" name="picture" placeholder="Votre image">
+		</div>
 		<br><br>
 
 		<label for="email">Email:</label>
+		<div class="center">
 		<input type="email" id="email" name="email">
+		</div>
 		<br><br>
 
 
 		<label for="password">Mot de passe:</label>
+		<div class="center">
 		<input type="password" id="password" name="password">
+		</div>
 		<br><br>
 
 		
 		<label for="role">role:</label>
+		<div class="center">
 		<select name="role">
 			<option>admin</option>
 			<option>modo</option>
 			<option>user</option>
 		</select>
+		</div>
+		<br><br>
 
-		<input type="submit"  value="Enregistrer">
-
-
+		<div class="btnsubmit">
+		<input type="submit"  class="linebuttons" value="ENREGISTRER">
+		</div>
+		<br><br>
 	</form>
-
+</div>
 
 
 <?php $this->stop('main_content') ?>
