c70bf846c07837178ad5faf45b6c5a233f9aa51a
Zaerowolf <ldarruau@gmail.com>
2016-12-21 11:58:46 +0100

    1158
    modification indesBack, remplacement de "gérer vos infos" par "gérer vos
    actualités"

--- app/Views/default/admin/indexBack.php
+++ app/Views/default/admin/indexBack.php
@@ -15,7 +15,7 @@
 	            Vous êtes sur la page d'administration de votre site web.<br> Vous pouvez, à l'aide de la barre de navigation en haut de page, gérer le contenu de votre site :
 	            <ul>
 	                <li><a href="<?=$this->url('user_list')?>">Gérer vos utilisateurs</a></li>
-	                <li><a href="<?=$this->url('updateInfo');?>">Gérer le contenu de votre site</a></li>
+	                <li><a href="<?=$this->url('updateInfo');?>">Gérer vos actualités</a></li>
 	                <li><a href="<?=$this->url('updateMaps')?>">Gérer votre carte Google maps</a></li>
 	                <li><a href=""> Lire les messages qui vous ont été envoyés dans la session "Contact"</a></li>
 	            </ul>
