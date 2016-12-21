0696fbff62edb8e5b3003c619c1cdfca1b47a6d0
hobbyon <romain.michler@hotmail.com>
2016-12-21 10:02:54 +0100

    maps ok

--- app/Views/default/admin/updateMaps.php
+++ app/Views/default/admin/updateMaps.php
@@ -1,8 +1,3 @@
-<?php
- 	if (empty($_SESSION)) {
-		$this->url('login');
-	} 
-	?>
 
 <?php $this->layout('layoutBack', ['title' => 'Gestion de la carte google maps']) ?>
 
@@ -62,18 +57,11 @@
 	  		
 
 			
-			function initMap() {
+				function initMap() {
 			  	
 			  	/* --------------AFFICHAGE DE LA MAP--------------- */
-			  	var geocoder;
+
 			  	var map;
-			  	var image = {
-				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
-				  size: new google.maps.Size(100, 100),
-				  origin: new google.maps.Point(0, 0),
-				  anchor: new google.maps.Point(17, 34),
-				  scaledSize: new google.maps.Size(40, 60),
-				};
 			  	
 			  	map = new google.maps.Map(document.getElementById('mapupdate'), {/* Ici on crée notre objet map*/
 				    center: {lat: 44.8404400, lng:-0.5805000}, // centrage de la carte à l'affichage
@@ -82,32 +70,52 @@
 				
 			  	/* --------------AFFICHAGE DES MARQUEURS--------------- */
 
+
+
+				var locations = [
+					<?php foreach($markers as $marker): ?>
+						['<?=$marker['title'];?>', '<?=$marker['lat'];?>', '<?=$marker['lng'];?>'],
+					<?php endforeach; ?>
+				];
+
 				
 	             //Création de l'icone
-	         	   //Affichage du marqueur
+	            var image = {
+				  url: "<?=$this->assetUrl('img/logo.png');?>" ,
+				  size: new google.maps.Size(100, 100),
+				  origin: new google.maps.Point(0, 0),
+				  anchor: new google.maps.Point(17, 34),
+				  scaledSize: new google.maps.Size(40, 60),
+				};
+	            
+				    
+	    	     //Affichage du marqueur
 	            for(i=0;i<locations.length;i++) { 
 				   marker = new google.maps.Marker({
 				   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
 				   map: map,
 				   icon: image, 
-	        
+				   
+					
+				   });
+				 // Création infobulle  
+	            	var infowindow = new google.maps.InfoWindow();  
 
-				google.maps.event.addListener(map, 'click', function(event) {
-				   placeMarker(event.latLng);
-				});
+					
+					google.maps.event.addListener(marker, 'click', (function(marker, i) {
+						return function() {
+							infowindow.setContent('Echangez ! <b>' + locations[i][0]);
+							infowindow.open(map, marker);
+						}
+						})(marker, i));
+				
+					 
 
-				function placeMarker(location) {
-				    var marker = new google.maps.Marker({
-				        position: location, 
-				        map: map
-				    });
-				}
-
-	         } 
-	            
-			   
-	    	   </script>
-
+				};
+				  
+			}
+			
+			</script>
 
 	  			<!-- .......................FIN DU SCRIPT MAPS................................... -->
 
