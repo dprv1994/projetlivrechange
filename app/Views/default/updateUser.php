182d6dd5771dff3db621d053e0b9d452dc6a9d51
Zaerowolf <ldarruau@gmail.com>
2016-12-21 18:48:16 +0100

    1848

--- app/Views/default/updateUser.php
+++ app/Views/default/updateUser.php
@@ -7,26 +7,36 @@
 	<form method="POST">
 		
 		<label  for="firstname">Prénom:</label>
+		<div class="center">
 		<input type="text" id="firstname" name="firstname">
+		</div>
 		<br><br>
 
 		<label for="lastname">Nom:</label>
+		<div class="center">
 		<input type="text" id="lastname" name="lastname">
+		</div>
 		<br><br>
 
 		
 
 		<label for="picture">Image:</label>
+		<div class="center">
 		<input type="file" id="picture" name="picture" placeholder="Votre image">
+		</div>
 		<br><br>
 
 		<label for="email">Email:</label>
+		<div class="center">
 		<input type="email" id="email" name="email">
+		</div>
 		<br><br>
 
 
 		<label for="password">Mot de passe:</label>
+		<div class="center">
 		<input type="password" id="password" name="password">
+		</div>
 		<br><br>
 
 		<input type="submit"  value="Enregistrer">
