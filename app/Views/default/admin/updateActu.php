aabcafcafd9e04bfcb13f67ce727a2a9334aa501
Zaerowolf <ldarruau@gmail.com>
2016-12-21 13:13:42 +0100

    1313

--- app/Views/default/admin/updateActu.php
+++ app/Views/default/admin/updateActu.php
@@ -12,7 +12,44 @@
 
 			<div>
 				<form method="POST">
+					<label for="event">Evènement :</label>
+					<input type="text" name="event" id="event">
+					<br><br>
+
+					<label for="who">Qui :</label>
+					<input type="text" name="who" id="who">
+					<br><br>
+
+					<label for="place">Lieu :</label>
+					<input type="text" name="place" id="place">
+					<br><br>
+
 					
+					<label for="date">Date :</label>
+						<select name="date_day">
+							<option value="" selected disabled>Jour</option>
+							<?php for($i=1;$i<=31;$i++): ?>
+								<option value="<?=$i;?>"><?=$i;?></option>
+							<?php endfor; ?>
+						</select>
+
+						<select name="date_month">
+							<option value="" selected disabled>Mois</option>
+							<?php for($i=1;$i<=12;$i++): ?>
+								<option value="<?=$i;?>"><?=$i;?></option>
+							<?php endfor; ?>
+						</select>
+
+						<select name="date_year">
+							<option value="" selected disabled>Année</option>
+							<?php for($i=date('Y');$i>=date('Y');$i--): ?>
+								<option value="<?=$i;?>"><?=$i;?></option>
+							<?php endfor; ?>
+						</select>
+
+				
+					<br><br>
+
 
 					<input type="submit"  value="Enregistrer">
 
