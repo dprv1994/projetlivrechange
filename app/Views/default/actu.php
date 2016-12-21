76fc82e1b340d1d26ea65cc89eb30a8d34b8e64a
Zaerowolf <ldarruau@gmail.com>
2016-12-21 11:44:17 +0100

    1143
    add text actu

--- app/Views/default/actu.php
+++ app/Views/default/actu.php
@@ -1,6 +1,20 @@
 <?php $this->layout('layout', ['title' => 'Actualités']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>Actualités</h1>
+<br><br>
+<body>
+	<h2>Actualités littéraires</h2>
+	
+	<br><br>
+
+	<div id="texte">
+		<p>Retrouvez ici les dernières actualités du monde du livre.<br> Conférences, dédicaces..
+		<br>Cette section est mise à jour régulièrement par nos administrateurs !
+		<br>
+	</div>
+
+</body>
+
+<br><br><br><br><br>
 
 <?php $this->stop('main_content') ?>
