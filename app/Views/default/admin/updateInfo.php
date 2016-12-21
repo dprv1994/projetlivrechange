23c5b2b6da2e230fb07aed4e08c467f24c6976fc
Zaerowolf <ldarruau@gmail.com>
2016-12-21 10:34:46 +0100

    1034
    création page updateInfo

---
+++ app/Views/default/admin/updateInfo.php
@@ -0,0 +1,23 @@
+
+<?php $this->layout('layoutBack', ['title' => 'Modifier les informations du site']) ?>
+
+<?php $this->start('main_content') ?>
+
+
+	<div class="container">
+
+			
+			<h2>Modifier le contenu de votre site</h2>
+			<br><br>
+
+			<div>
+				<form method="POST">
+					
+
+					<input type="submit"  value="Enregistrer">
+
+				</form>
+	        </div>
+	</div>	
+
+<?php $this->stop('main_content') ?>
